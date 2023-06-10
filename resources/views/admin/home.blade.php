d4bc2fcbe5b2e7bae628035f945bafce91e904ce
naglaa19 <naglaaassem43@gmail.com>
2023-06-10 21:53:11 +0300

    home admin

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -9,7 +9,7 @@
         <canvas id="myChart"></canvas>
       </div>
       
-      <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.1/chart.min.js"></script>
       
       <script>
         const ctx = document.getElementById('myChart');
